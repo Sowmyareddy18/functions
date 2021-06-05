2d3b23466823bee016bbba35a779d9e88d6fc4ae
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-06-05 10:39:26 +0530

    Add files via upload

---
+++ add_fn.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int add(int x,int y)
+{
+	return x+y;
+}
+int main()
+{
+	int x=20,y=30;
+	int result;
+	result=add(x,y);
+	cout<<result;
+	return 0;
+}
